708d4ad09adcc2e93dbdbacbb506e2cc5fa6c0d6
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2016-03-27 14:26:19 +0100

    Added Star Ratings

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,7 +31,9 @@
     <link href="http://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet" type="text/css">
 
-
+    <link rel="stylesheet" href="/css/star-rating.css" media="all" rel="stylesheet" type="text/css"/>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="../js/star-rating.js" type="text/javascript"></script>
 
 
 
@@ -194,11 +196,20 @@
     $( function()
     {
 
+        
+
+
         Search();
 
         Browse();
 
         LoadPage();
+
+
+
+
+
+        //Rating();
 
     });
 
@@ -252,6 +263,59 @@
 
         });
     }
+
+
+   /* function Rating()
+    {
+        $("#input-21f").rating({
+            starCaptions: function(val) {
+                if (val < 3) {
+                    return val;
+                } else {
+                    return 'high';
+                }
+            },
+            starCaptionClasses: function(val) {
+                if (val < 3) {
+                    return 'label label-danger';
+                } else {
+                    return 'label label-success';
+                }
+            },
+            hoverOnClear: false
+        });
+
+        $('#rating-input').rating({
+            min: 0,
+            max: 5,
+            step: 1,
+            size: 'lg',
+            showClear: false
+        });
+
+        $('#btn-rating-input').on('click', function() {
+            $('#rating-input').rating('refresh', {
+                showClear:true,
+                disabled: !$('#rating-input').attr('disabled')
+            });
+        });
+
+
+        $('.btn-danger').on('click', function() {
+            $("#kartik").rating('destroy');
+        });
+
+        $('.btn-success').on('click', function() {
+            $("#kartik").rating('create');
+        });
+
+        $('#rating-input').on('rating.change', function() {
+            alert($('#rating-input').val());
+        });
+
+
+        $('.rb-rating').rating({'showCaption':true, 'stars':'3', 'min':'0', 'max':'3', 'step':'1', 'size':'xs', 'starCaptions': {0:'status:nix', 1:'status:wackelt', 2:'status:geht', 3:'status:laeuft'}});
+    }*/
 
 </script>
 </body>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,24 @@
 
 @section('content')
 
-
+        <!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+    <div class="container">
+        <h1>Lets Cook</h1>
+         <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+    </div>
+</div>
 
 <div class="wrapper">
     <div class="container">
-        <h1>Featured Recipes</h1>
+        <h1>Latest Recipes</h1>
        <!-- <ul class="grid-nav">
             <li><a href="#" data-id="two-columns" >2 Columns</a></li>
             <li><a href="#" data-id="three-columns" class="active">3 Columns</a></li>
             <li><a href="#" data-id="four-columns">4 Columns</a></li>
         </ul>-->
 
-        <hr />
+
 
         <div id="two-columns" class="grid-container" >
             <ul class="rig columns-2">
@@ -33,9 +39,9 @@
         <!--/#two-columns-->
 
 
-            <div id="three-columns" class="grid-container" style="display:block;">
+            <div id="four-columns" class="grid-container" style="display:block;">
 
-                <ul class="rig columns-3">
+                <ul class="rig columns-4">
 
                     @foreach ( $featured as $value )
 
@@ -43,8 +49,13 @@
 
                             <li>
                                 <img src="http://img.youtube.com/vi/{{ $value[ 'link' ] }}/mqdefault.jpg" />
-                                <h3> {{ $value[ 'title' ] }} </h3>
-                                <p> {{ $value[ 'description' ]  }} </p>
+                                <h4> {{ $value[ 'title' ] }} </h4>
+                                <div id="rating" class="blurb">
+                                    <input id="input-1" value="4" type="number" class="rating" readonly="true" min=0 max=5 step=0.2 data-size="xs">
+                                </div>
+                                <span class="description"> {{ $value[ 'description' ]  }} </span>
+                                <div id="author" class="blurb">Author:</div>
+
                             </li>
 
                         </a>
